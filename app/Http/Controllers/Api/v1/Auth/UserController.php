dbb7117b43a9f92ecb2424f53409601aefb15f45
brnaldomesi <brnaldomesi1@gmail.com>
2020-12-15 01:28:19 +0900

    Implement Auth module

---
+++ app/Http/Controllers/Api/v1/Auth/UserController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+  public function checkUser(Request $request): Response
+  {
+    $request->validate([
+      'email' => 'required|email'
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if($user) {
+      return response(['status' => 200, 'msg' => 'Email exist']);
+    } else {
+      return response(['status' => 404, 'msg' => 'Email not exist']);
+    }
+  }
+
+  public function signup(Request $request): Response
+  {
+    $request->validate([
+      'email' => 'required|email',
+      'password' => 'required',
+      'deviceName' => 'required'
+    ]);
+
+    $user = new User;
+    $user->email = $request->email;
+    $user->password = Hash::make($request->password);
+    $user->save();
+
+    return response(['auth-token' => $user->createToken($request->deviceName)->plainTextToken ]);
+  }
+
+  public function login(Request $request): Response
+  {
+    $request->validate([
+      'email' => 'required|email',
+      'password' => 'required',
+      'deviceName' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+      throw ValidationException::withMessages([
+        'email' => ['The provided credentials are incorrect.'],
+      ]);
+    }
+
+    return response(['auth-token' => $user->createToken($request->deviceName)->plainTextToken ]);
+  }
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use App\Http\Controllers\Api\v1\Auth\UserController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('auth')->group(function () {
+  Route::post('/checkUser', [UserController::class , 'checkUser']);
+  Route::post('/login', [UserController::class , 'login']);
+  Route::post('/signup', [UserController::class , 'signup']);
+});
